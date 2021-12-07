8d77c007551a2610a9e45d1a879fc526c4534fc8
antoinette-kislig <antoinette_hoang@yahoo.fr>
2021-12-07 15:40:54 +0100

    Affichage d'une liste et tri de cette liste

---
+++ GestionDeListe.py
@@ -0,0 +1,8 @@
+
+if __name__ == '__main__':
+    liste = [17, 38, 10, 25, 72]
+    print("Liste à trier : " + str(liste))
+
+    # Trier la liste et l'afficher
+    liste_triee = sorted(liste)
+    print("Liste triée : " + str(liste_triee))
